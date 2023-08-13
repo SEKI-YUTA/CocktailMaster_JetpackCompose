5ebe13f9bb148e66b581a6f901a0a5bf613cd2e8
SEKI-YUTA <zard.since1991@gmail.com>
2023-08-13 16:42:09 +0900

    デフォルト値を追加

--- app/src/main/java/com/example/cocktailmaster/data/model/Cocktail_Data.kt
+++ app/src/main/java/com/example/cocktailmaster/data/model/Cocktail_Data.kt
@@ -1,13 +1,15 @@
 package com.example.cocktailmaster.data.model
 
 import com.example.cocktailmaster.ui.model.Cocktail_UI
+import kotlinx.serialization.Serializable
 
 // データソースから引っ張ってくる時のデータ型そのためUIに表示させる時はtoUIModel()を呼び出してUI用のデータ型に変換する
+@Serializable
 data class Cocktail_Data(
     val name: String,
-    val imageUri: String?,
+    val imageUri: String? = null,
     val description: String?,
-    val craftable: Boolean,
+    val craftable: Boolean = false,
     val ingredients: List<CocktailIngredient_Data>
 ) {
     fun toUIModel(): Cocktail_UI {

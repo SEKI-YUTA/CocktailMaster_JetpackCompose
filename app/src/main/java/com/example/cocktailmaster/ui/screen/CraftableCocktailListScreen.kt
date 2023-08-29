60c8751b1de7f67d5f2ee8d20a720e3635992c29
SEKI-YUA <zard.since1991@gmail.com>
2023-08-29 23:55:40 +0900

    作る事ができるカクテルリストにも絞り込み機能を追加

--- app/src/main/java/com/example/cocktailmaster/ui/screen/CraftableCocktailListScreen.kt
+++ app/src/main/java/com/example/cocktailmaster/ui/screen/CraftableCocktailListScreen.kt
@@ -2,6 +2,7 @@ package com.example.cocktailmaster.ui.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -22,6 +29,7 @@ import com.example.cocktailmaster.ui.Screen
 import com.example.cocktailmaster.ui.component.CenterMessage
 import com.example.cocktailmaster.ui.component.CocktailListItem
 import com.example.cocktailmaster.ui.component.LoadingMessage
+import com.example.cocktailmaster.ui.component.MyDropDownMenu
 import com.example.cocktailmaster.ui.viewmodels.MainViewModel
 
 @Composable
@@ -29,6 +37,13 @@ fun CraftableCocktailListScreen(navController: NavHostController, viewModel: Mai
     val craftableCocktailList = viewModel.craftableCocktailList.collectAsState().value
     val isLoading = viewModel.isLoading.collectAsState().value
     val isFetchfailed = viewModel.isFetchFailed.collectAsState().value
+    val categories by remember(craftableCocktailList) {
+        derivedStateOf {
+            listOf("すべて") + LinkedHashSet(craftableCocktailList.map { it.category }).toMutableList()
+        }
+    }
+    var userSelectCategory by remember { mutableStateOf(categories[0]) }
+
     viewModel.setCurrentScreen(Screen.CraftableCocktailListScreen)
     LaunchedEffect(key1 = true) {
         println("launchedEffect")
@@ -36,14 +51,31 @@ fun CraftableCocktailListScreen(navController: NavHostController, viewModel: Mai
     }
     Box {
         Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+            ) {
+                Text("絞り込み: ", modifier = Modifier.padding(end = 8.dp))
+                MyDropDownMenu(
+                    items = categories,
+                    selectedVal = userSelectCategory,
+                ) {
+                    userSelectCategory = it
+                }
+            }
             Text(
                 text = stringResource(id = R.string.can_make_cocktail_str),
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(
+                    horizontal = 16.dp,
+                    vertical = 8.dp
+                ),
                 style = TextStyle(fontSize = 22.sp)
             )
             LazyColumn {
                 items(craftableCocktailList) { cocktail_UI ->
-                    CocktailListItem(cocktail_UI = cocktail_UI)
+                    if(userSelectCategory == "すべて" || userSelectCategory == cocktail_UI.category) {
+                        CocktailListItem(cocktail_UI = cocktail_UI)
+                    }
                 }
             }
 

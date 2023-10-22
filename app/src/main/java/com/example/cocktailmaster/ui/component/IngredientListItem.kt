f7c7903f6cc4d718deadaa57e2e8d59cf282f392
SEKI-YUTA <zard.since1991@gmail,com>
2023-10-22 23:01:47 +0900

    材料追加画面ですでに持っている材料はカウントをバッジに表示するように変更

--- app/src/main/java/com/example/cocktailmaster/ui/component/IngredientListItem.kt
+++ app/src/main/java/com/example/cocktailmaster/ui/component/IngredientListItem.kt
@@ -1,11 +1,15 @@
 package com.example.cocktailmaster.ui.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -13,119 +17,157 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.cocktailmaster.R
 import com.example.cocktailmaster.data.DemoData
 import com.example.cocktailmaster.ui.model.CocktailIngredient_UI
 import com.example.cocktailmaster.ui.theme.CocktailMasterTheme
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun IngredientListItem(
     modifier: Modifier = Modifier,
     ingredient_UI: CocktailIngredient_UI,
     tailIcon: ImageVector? = null,
     enableContextMenu: Boolean = true,
+    ownedCount: Int = 0,
+    showBadgeWhenOwned: Boolean = false,
     onIconTapAction: (CocktailIngredient_UI) -> Unit = {},
     onDeleteAction: (CocktailIngredient_UI) -> Unit = {},
     onEditAction: (CocktailIngredient_UI) -> Unit = {}
 ) {
     val menuShowing = remember { mutableStateOf(false) }
     val isShowingDialog = remember { mutableStateOf(false) }
-    Card(
-        modifier = modifier
-            .padding(8.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onLongPress = {
-                        if(enableContextMenu) {
-                            // 削除と編集ボタンを表示する処理を書く
-                            println("Long Pressed")
-                            menuShowing.value = true
+    ConstraintLayout(
+    ) {
+        val (cardRef, badgeRef) = createRefs()
+        Card(
+            modifier = modifier
+                .padding(8.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onLongPress = {
+                            if (enableContextMenu) {
+                                // 削除と編集ボタンを表示する処理を書く
+                                println("Long Pressed")
+                                menuShowing.value = true
+                            }
                         }
-                    }
-                )
-            },
+                    )
+                }
+                .constrainAs(cardRef) {
+                    linkTo(start = parent.start, top = parent.top, end = parent.end, bottom = parent.bottom)
+                },
         ) {
-        Row(
-            modifier = modifier.padding(start = 16.dp, end = 8.dp, top = 8.dp, bottom = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f),
-                verticalArrangement = Arrangement.Center
+            Row(
+                modifier = modifier.padding(start = 16.dp, end = 8.dp, top = 8.dp, bottom = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = ingredient_UI.longName,
-                    style = TextStyle(fontSize = 20.sp),
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-                if (ingredient_UI.description != "") {
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    verticalArrangement = Arrangement.Center
+                ) {
                     Text(
-                        text = ingredient_UI.description,
-                        style = TextStyle(fontSize = 16.sp),
-                        maxLines = 2
+                        text = ingredient_UI.longName,
+                        style = TextStyle(fontSize = 20.sp),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1
+                    )
+                    if (ingredient_UI.description != "") {
+                        Text(
+                            text = ingredient_UI.description,
+                            style = TextStyle(fontSize = 16.sp),
+                            maxLines = 2
+                        )
+                    }
+                }
+                Text("${ingredient_UI.vol}${stringResource(id = R.string.alcohol_volume_unit)}")
+                if (tailIcon != null) {
+                    IconButton(onClick = { onIconTapAction(ingredient_UI) }) {
+                        Icon(imageVector = tailIcon, contentDescription = null)
+                    }
+                }
+            }
+            if(menuShowing.value) {
+                DropdownMenu(
+                    expanded = menuShowing.value,
+                    onDismissRequest = {
+                        menuShowing.value = false
+                    }) {
+                    DropdownMenuItem(
+                        text = {
+                            Text(stringResource(R.string.edit_str))
+                        },
+                        onClick = {
+                            isShowingDialog.value = true
+                            menuShowing.value = false
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(stringResource(R.string.remove_str))
+                        },
+                        onClick = {
+                            onDeleteAction(ingredient_UI)
+                            menuShowing.value = false
+                        }
                     )
                 }
             }
-            Text("${ingredient_UI.vol}${stringResource(id = R.string.alcohol_volume_unit)}")
-            if (tailIcon != null) {
-                IconButton(onClick = { onIconTapAction(ingredient_UI) }) {
-                    Icon(imageVector = tailIcon, contentDescription = null)
-                }
-            }
-        }
-        if(menuShowing.value) {
-            DropdownMenu(
-                expanded = menuShowing.value,
-                onDismissRequest = {
-                    menuShowing.value = false
-                }) {
-                DropdownMenuItem(
-                    text = {
-                        Text(stringResource(R.string.edit_str))
-                    },
-                    onClick = {
-                        isShowingDialog.value = true
-                        menuShowing.value = false
-                    }
-                )
-                DropdownMenuItem(
-                    text = {
-                        Text(stringResource(R.string.remove_str))
-                    },
-                    onClick = {
-                        onDeleteAction(ingredient_UI)
-                        menuShowing.value = false
+            if(isShowingDialog.value) {
+                AddEditIngredientDialog(
+                    isShowingDialog = isShowingDialog,
+                    currentIngredient = ingredient_UI,
+                    onDoneEvent = { ingredient_ui ->
+                        onEditAction(ingredient_ui)
+                        isShowingDialog.value = false
                     }
                 )
             }
         }
-        if(isShowingDialog.value) {
-            AddEditIngredientDialog(
-                isShowingDialog = isShowingDialog,
-                currentIngredient = ingredient_UI,
-                onDoneEvent = { ingredient_ui ->
-                    onEditAction(ingredient_ui)
-                    isShowingDialog.value = false
-                }
+        if(showBadgeWhenOwned && ownedCount > 0) {
+            CountBadge(
+                modifier = Modifier.constrainAs(badgeRef){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
+                text = "$ownedCount"
             )
         }
+    }
+}
+
+@Composable
+fun CountBadge(modifier: Modifier = Modifier,text: String) {
+    Box(
+        modifier = modifier
+            .size(32.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.inversePrimary),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = text, style = TextStyle(fontWeight = FontWeight.Bold))
     }
 }
 
@@ -136,7 +178,9 @@ fun IngredientListItemPreview_Light() {
     CocktailMasterTheme {
         Surface {
             IngredientListItem(
+                ownedCount = 1,
                 ingredient_UI = data,
+                showBadgeWhenOwned = true,
                 tailIcon = Icons.Default.Add
             )
         }
@@ -150,6 +194,8 @@ fun IngredientListItemPreview_Night() {
     CocktailMasterTheme {
         Surface {
             IngredientListItem(
+                ownedCount = 1,
+                showBadgeWhenOwned = true,
                 ingredient_UI = data,
             )
         }
--- app/src/main/java/com/example/cocktailmaster/ui/screen/AddCocktailIngredientScreen.kt
+++ app/src/main/java/com/example/cocktailmaster/ui/screen/AddCocktailIngredientScreen.kt
@@ -3,29 +3,19 @@
 package com.example.cocktailmaster.ui.screen
 
 import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -35,13 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.example.cocktailmaster.R
-import com.example.cocktailmaster.data.ConstantValues
 import com.example.cocktailmaster.ui.Screen
 import com.example.cocktailmaster.ui.component.AddEditIngredientDialog
 import com.example.cocktailmaster.ui.component.CenterMessage
@@ -53,13 +40,21 @@ import com.example.cocktailmaster.ui.viewmodels.MainViewModel
 
 @Composable
 fun AddCocktailIngredientScreen(viewModel: MainViewModel) {
-    var isShowingDialog = remember { mutableStateOf(false) }
-    var currentIngredient = remember { mutableStateOf<CocktailIngredient_UI?>(null) }
-    var userInputName = remember { mutableStateOf("") }
+    val isShowingDialog = remember { mutableStateOf(false) }
+    val currentIngredient = remember { mutableStateOf<CocktailIngredient_UI?>(null) }
+    val userInputName = remember { mutableStateOf("") }
     val ingredientList = viewModel.ingredientList.collectAsState().value
     val isLoading = viewModel.isLoading.collectAsState().value
-    val isFetchfailed = viewModel.isFetchFailed.collectAsState().value
+    val isFetchFailed = viewModel.isFetchFailed.collectAsState().value
     val context = LocalContext.current
+    val ownedIngredientList = viewModel.ownedCocktailIngredients.collectAsState().value
+    val ownedIngredientList_LogName by remember(ownedIngredientList) {
+        derivedStateOf {
+            ownedIngredientList.map { cocktailingredientUi ->
+                cocktailingredientUi.longName
+            }
+        }
+    }
 
     val categories by remember(ingredientList) {
         derivedStateOf {
@@ -93,6 +88,8 @@ fun AddCocktailIngredientScreen(viewModel: MainViewModel) {
                             ingredient_UI = ingredient_ui,
                             tailIcon = Icons.Default.Add,
                             enableContextMenu = false,
+                            showBadgeWhenOwned = true,
+                            ownedCount = checkContainsCount(ownedIngredientList_LogName, ingredient_ui.longName),
                             onIconTapAction = {
                                 currentIngredient.value = it
                                 isShowingDialog.value = true
@@ -103,7 +100,7 @@ fun AddCocktailIngredientScreen(viewModel: MainViewModel) {
             }
             if (isLoading) {
                 LoadingMessage()
-            } else if(isFetchfailed) {
+            } else if(isFetchFailed) {
                 CenterMessage(
                     message = stringResource(R.string.fetch_failed_message),
                     icon = Icons.Default.Refresh,
@@ -132,4 +129,11 @@ fun AddCocktailIngredientScreen(viewModel: MainViewModel) {
             }
         }
     }
+}
+
+fun checkContainsCount(list: List<String>, target: String): Int {
+    val count = list.stream().filter {
+        it == target
+    }.count()
+    return count.toInt()
 }

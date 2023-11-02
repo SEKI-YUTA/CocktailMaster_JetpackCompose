ddd52d9889cf8745abda3caa9d837624cfb925c2
SEKI-YUA <zard.since1991@gmail.com>
2023-11-02 23:37:35 +0900

    ドロップダウンメニューの矢印アイコン以外を押してもドロップダウンが開くように変更

--- app/src/main/java/com/example/cocktailmaster/ui/component/MyDropDownMenu.kt
+++ app/src/main/java/com/example/cocktailmaster/ui/component/MyDropDownMenu.kt
@@ -3,36 +3,22 @@
 package com.example.cocktailmaster.ui.component
 
 import android.content.res.Configuration
-import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,13 +26,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.cocktailmaster.data.ConstantValues
 import com.example.cocktailmaster.ui.theme.CocktailMasterTheme
 
@@ -57,23 +39,30 @@ fun MyDropDownMenu(
     selectedVal: String,
     onItemSelected: (String) -> Unit
 ) {
-    var expanded by remember { mutableStateOf(false)}
-
+    var expanded by remember { mutableStateOf(false) }
+    val rotateDegree by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f, label = ""
+    )
     Column(
         modifier = modifier,
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 8.dp)
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .clickable {
+                    expanded = true
+                }
         ) {
             Text(selectedVal, modifier = Modifier.weight(1f))
-            IconButton(onClick = {
-                expanded = true
-            }) {
+            Box(
+                modifier = Modifier
+                    .rotate(rotateDegree)
+                    .wrapContentSize()
+            ) {
                 Icon(
-                    imageVector =
-                    if(expanded) Icons.Filled.ArrowDropDown else Icons.Filled.ArrowDropDown,
-                    contentDescription = null
+                    Icons.Rounded.ArrowDropDown,
+                    contentDescription = ""
                 )
             }
         }
@@ -85,12 +74,15 @@ fun MyDropDownMenu(
             }
         ) {
             items.map {
-                DropdownMenuItem(text = {
-                    Text(it)
-                }, onClick = {
-                    onItemSelected(it)
-                    expanded = false
-                })
+                DropdownMenuItem(
+                    text = {
+                        Text(it)
+                    },
+                    onClick = {
+                        onItemSelected(it)
+                        expanded = false
+                    }
+                )
             }
         }
     }

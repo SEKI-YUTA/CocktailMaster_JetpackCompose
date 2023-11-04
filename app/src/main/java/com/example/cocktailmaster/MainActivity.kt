ae6f995e233b60a55160be614d350ee79e4b192a
SEKI-YUA <zard.since1991@gmail.com>
2023-11-04 17:11:55 +0900

    リポジトリを渡すプロバイダーを作成してMainActivityに追加

--- app/src/main/java/com/example/cocktailmaster/MainActivity.kt
+++ app/src/main/java/com/example/cocktailmaster/MainActivity.kt
@@ -13,13 +13,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CocktailMasterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainHost()
+            RepositoryProvider {
+                CocktailMasterTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        MainHost()
+                    }
                 }
             }
         }

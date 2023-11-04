ee342a6fedb4555c1d9d7f0f5c54136074eb252f
SEKI-YUA <zard.since1991@gmail.com>
2023-11-04 16:04:13 +0900

    プレビュー用のアノテーションを宣言

---
+++ app/src/main/java/com/example/cocktailmaster/util/CocktailMasterAnnotations.kt
@@ -0,0 +1,11 @@
+package com.example.cocktailmaster.util
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.TABLET)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, device = Devices.TABLET, uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class CocktailMasterPreviewAnnotation

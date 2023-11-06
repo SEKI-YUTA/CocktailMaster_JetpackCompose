0648fb7dc2205740ee7eafa41cbd6a3d07374822
SEKI-YUTA <zard.since1991@gmail.com>
2023-11-07 01:09:56 +0900

    ネット接続を確認する関数の仕様を変更

--- app/src/main/java/com/example/cocktailmaster/util/AppUtil.kt
+++ app/src/main/java/com/example/cocktailmaster/util/AppUtil.kt
@@ -2,27 +2,22 @@ package com.example.cocktailmaster.util
 
 import android.content.Context
 import android.net.ConnectivityManager
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class AppUtil {
     companion object {
-        fun checkNetworkConnection(context: Context, status: MutableStateFlow<Boolean>) {
+        fun checkNetworkConnection(context: Context, onResult: (Boolean) -> Unit) {
             val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork = cm.activeNetworkInfo
             cm.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: android.net.Network) {
                     super.onAvailable(network)
-                    status.value = true
-                    println("network status changed ${status.value}")
+                    onResult(true)
                 }
 
                 override fun onLost(network: android.net.Network) {
                     super.onLost(network)
-                    status.value = false
-                    println("network status changed ${status.value}")
+                    onResult(false)
                 }
             })
-            status.value = activeNetwork != null && activeNetwork.isConnected
         }
     }
 }

ea474ef40b68ad930eb7812928b69143cf0aee2c
SEKI-YUTA <zard.since1991@gmail.com>
2023-11-07 09:28:01 +0900

    ネット接続がなくなった際のメッセージを追加

--- app/src/main/java/com/example/cocktailmaster/MainHost.kt
+++ app/src/main/java/com/example/cocktailmaster/MainHost.kt
@@ -2,14 +2,21 @@
 
 package com.example.cocktailmaster
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -95,6 +102,24 @@ fun MainHost(
                         )
                     )
                 CraftableCocktailListScreen(viewModel = craftableCocktailListScreenViewModel)
+            }
+        }
+        if(!mainViewState.isNetworkConnected) {
+            Popup() {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.4f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    AlertDialog(
+                        title = {
+                            Text(text = "ネット環境を確認してください")
+                        },
+                        onDismissRequest = {},
+                        confirmButton = {}
+                    )
+                }
             }
         }
     }

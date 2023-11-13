bbc302925c31118ee6f6b8e01abad128c3d1cfc6
SEKI-YUTA <zard.since1991@gmail.com>
2023-11-13 22:06:48 +0900

    データモデルにデフォルト値を追加

--- app/src/main/java/com/yuta/cocktailmaster/ui/model/Cocktail_UI.kt
+++ app/src/main/java/com/yuta/cocktailmaster/ui/model/Cocktail_UI.kt
@@ -2,11 +2,11 @@ package com.yuta.cocktailmaster.ui.model
 
 data class Cocktail_UI(
     val cocktailId: Int = 0,
-    val name: String,
+    val name: String = "",
     val description: String = "",
     val vol: Int = 0,
     val ingredientCount: Int = 0,
     val category: String = "",
     val cocktailCategoryId: Int = 0,
-    val ingredients: List<CocktailIngredient_UI>
+    val ingredients: List<CocktailIngredient_UI> = emptyList()
 )

311bd46b68c132e4476862d8f0bc3a289abb9636
SEKI-YUTA <zard.since1991@gmail.com>
2023-11-18 15:46:53 +0900

    非同期な関数のテストを追加

--- app/src/test/java/com/yuta/cocktailmaster/ui/viewmodels/CraftableCocktailListScreenViewModelTest.kt
+++ app/src/test/java/com/yuta/cocktailmaster/ui/viewmodels/CraftableCocktailListScreenViewModelTest.kt
@@ -1,9 +1,12 @@
 package com.yuta.cocktailmaster.ui.viewmodels
 
+import com.yuta.cocktailmaster.data.DemoData
 import com.yuta.cocktailmaster.data.interfaces.CocktailApiRepository
+import com.yuta.cocktailmaster.data.repository.CocktailApiRepository_FakeImpl
 import com.yuta.cocktailmaster.ui.model.CocktailIngredient_UI
 import io.kotest.matchers.shouldBe
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class CraftableCocktailListScreenViewModelTest {
@@ -15,6 +18,20 @@ class CraftableCocktailListScreenViewModelTest {
             apiRepository = apiRepository,
             ingredientList = ingredientList
         )
+    }
+
+    @Test
+    fun `カクテルを取得できる`() = runTest {
+        val viewModel = CraftableCocktailListScreenViewModelFactory(
+            apiRepository = CocktailApiRepository_FakeImpl(),
+            ingredientList = DemoData.ingredientList.map { it.toUIModel() }
+        )
+
+        viewModel.onEvent(CraftableCocktailListScreenEvent.FetchCocktailData)
+        val viewState = viewModel.viewState.value
+
+        viewState.craftableCocktailList shouldBe DemoData.cocktailList.map { it.toUIModel() }
+        viewState.allCocktailList shouldBe DemoData.cocktailList.map { it.toUIModel() }
     }
 
     @Test

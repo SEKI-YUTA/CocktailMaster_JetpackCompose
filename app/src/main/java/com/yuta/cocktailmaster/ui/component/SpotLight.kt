104cd3f2407a4dfe51c3ca24701ad768f896e81e
SEKI-YUTA <zard.since1991@gmail.com>
2023-11-23 17:44:50 +0900

    チュートリアルが再び表示されるバグを修正

--- app/src/main/java/com/yuta/cocktailmaster/ui/component/SpotLight.kt
+++ app/src/main/java/com/yuta/cocktailmaster/ui/component/SpotLight.kt
@@ -1,9 +1,18 @@
 package com.yuta.cocktailmaster.ui.component
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -11,13 +20,12 @@ import androidx.compose.ui.graphics.ClipOp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -38,68 +46,55 @@ fun SpotLight(
         lineHeight = TextUnit(30f, TextUnitType.Sp)
     )
 
-    Canvas(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTapGestures {
-                    println("area tapped")
-                    if(isLast){
-                        print("mark finished")
-                        onMarkOnboardingFinished(true)
-                    } else {
-                        onAreaTapped()
-                    }
-                }
-            }
+            .clickable {
+                if (!isLast) onAreaTapped()
+            },
+        contentAlignment = Alignment.Center
     ) {
-        val spotlightPath = Path().apply {
-            addRect(actual)
-        }
-        clipPath(
-            path = spotlightPath,
-            clipOp = ClipOp.Difference
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+
         ) {
-            drawRect(color = Color.Black.copy(alpha = 0.8f))
-        }
-        if(isLast) {
-            val width = size.width
-            val height = size.height
-            val textLayoutResult = textMeasure.measure(
-                text = AnnotatedString("さぁ使ってみましょう!"),
-                style = textStyle
-            )
-            val textSize = textLayoutResult.size
-            drawText(
-                textMeasure,
-                text = "さぁ使ってみましょう!",
-                topLeft = Offset(
-                    x = (width - textSize.width) / 2,
-                    y = (height - textSize.height) / 2
-                ),
-                style = textStyle
-            )
-        } else {
-
-            val yPos = when (textAreaPosition) {
-                TextAreaPosition.ABOVE -> actual.top - (textStyle.fontSize.value * 2) - 40
-                TextAreaPosition.BELOW -> actual.bottom + 30
+            val spotlightPath = Path().apply {
+                addRect(actual)
             }
-            drawText(
-                textMeasure,
-                text = text,
-                topLeft = Offset(
-                    x = 40f,
-                    y = yPos
-                ),
-                style = TextStyle(
-                    fontSize = 26.sp,
-                    color = Color.White,
-                    lineHeight = TextUnit(30f, TextUnitType.Sp)
+            clipPath(
+                path = spotlightPath,
+                clipOp = ClipOp.Difference
+            ) {
+                drawRect(color = Color.Black.copy(alpha = 0.8f))
+            }
+            if (!isLast) {
+                val yPos = when (textAreaPosition) {
+                    TextAreaPosition.ABOVE -> actual.top - (textStyle.fontSize.value * 2) - 40
+                    TextAreaPosition.BELOW -> actual.bottom + 30
+                }
+                drawText(
+                    textMeasure,
+                    text = text,
+                    topLeft = Offset(
+                        x = 40f,
+                        y = yPos
+                    ),
+                    style = TextStyle(
+                        fontSize = 26.sp,
+                        color = Color.White,
+                        lineHeight = TextUnit(30f, TextUnitType.Sp)
+                    )
                 )
-            )
+            }
         }
-
+        if (isLast) {
+            Button(onClick = { onMarkOnboardingFinished(true) }) {
+                Text(text = "さぁ使ってみましょう!")
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(Icons.Rounded.ArrowForward, contentDescription = null)
+            }
+        }
     }
 }
 
